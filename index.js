aa98e0be85dd07977f00d5e49ef55d67012d5e6d
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-03-24 16:44:22 +0000

    Retrieved access token

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const querystring = require('querystring');
+const http = require('request');
 const argv = require('minimist')(process.argv.slice(2));
 const FileStore = require('session-file-store')(session);
 const app = express();
 
+const redirectUrl = `http://localhost:${port}/auth`;
 const loginUrl = 'https://auth.getmondo.co.uk/?' + querystring.stringify({
   'client_id': argv.clientId,
-  'redirect_uri': `http://localhost:${port}/auth`,
+  'redirect_uri': redirectUrl,
   'response_type': 'code'
 });
 
@@ -26,6 +28,34 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+app.get('/auth', (request, response) => {
+  const formData = {
+    'grant_type': 'authorization_code',
+    'client_id': argv.clientId,
+    'client_secret': argv.clientSecret,
+    'redirect_uri': redirectUrl,
+    'code': request.query.code
+  };
+
+  http.post({
+    url: 'https://api.getmondo.co.uk/oauth2/token',
+    form: formData},
+    (err, _, body) => {
+      if (err) {
+        return console.error('upload failed:', err);
+      } else {
+        const jsonResponse = JSON.parse(body);
+        if (jsonResponse.error) {
+          console.error(jsonResponse);
+        } else {
+          console.log('Access token returned:', jsonResponse.access_token);
+          request.session.token = jsonResponse.access_token;
+          response.redirect('/');
+        }
+      }
+    });
+});
 
 app.get('/', (request, response) => {
   const accessToken = request.session.token;
