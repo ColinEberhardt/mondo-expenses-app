504b22c41f61e002742639b507bcbd2253dd03ec
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-03-24 16:22:42 +0000

    redirect to the login page

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const port = 5000;
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const querystring = require('querystring');
+const argv = require('minimist')(process.argv.slice(2));
 const FileStore = require('session-file-store')(session);
 const app = express();
+
+const loginUrl = 'https://auth.getmondo.co.uk/?' + querystring.stringify({
+  'client_id': argv.clientId,
+  'redirect_uri': `http://localhost:${port}/auth`,
+  'response_type': 'code'
+});
 
 // configure the session
 app.use(session({
@@ -12,16 +21,22 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.set('port', 5000);
+app.set('port', port);
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
+  const accessToken = request.session.token;
+  if (!accessToken) {
+    response.redirect(loginUrl);
+    return;
+  }
+
   response.render('index');
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(port, () => {
+  console.log('Node app is running on port', port);
 });
